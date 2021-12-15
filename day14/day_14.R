75bc3ad0ea76dc568b48747332eeef895bf275c3
arjunkhetia66 <arjun.khetia@justice.gov.uk>
2021-12-15 10:47:08 +0000

    debug help

--- day14/day_14.R
+++ day14/day_14.R
@@ -1,5 +1,6 @@
 #day 14
 source("utils.R")
+source("day14/debug_help.R")
 
 #test template
 test_template <- "NNCB"
@@ -20,41 +21,9 @@ pairs_insert <- real_pairs
 #split template up
 split_template <- strsplit(template,"")[[1]]
 
-#function to count how many of a particular letter in given template
-count_letters <- function(template_ins, letter) {
-  return((sum(template_ins %in% letter)))
-}
-
-#function to count number of each letter in template
-count_letters_bigger <- function(template){
-  split_template <- strsplit(template,"")[[1]]
-  #keep count of letters in template
-  letters_count <- split_template %>% tibble()
-  names(letters_count) = "letters"
-  letters_count <- letters_count %>% distinct(letters)
-  
-  letters_count <- letters_count %>% 
-    mutate(counts = as.numeric(unlist(purrr::map(letters, ~count_letters(split_template, .)))))
-  
-  return(letters_count)
-}
-
 #function to lookup which letter is inserted
 which_insert <- function(pair_checked, pair_insert_lookup = pairs_insert){
   return(pair_insert_lookup %>% filter(pair == pair_checked) %>% select(insert) %>% as.character())
-}
-
-#apply lookup to pairs
-insertion <- function(split_template){
-  split_template_paired <- runner(split_template, k = 2, f = paste, collapse = "")
-  inserts <- purrr::map(split_template_paired, ~which_insert(.)) %>% unlist()
-  j=1
-  for (i in (2:length(inserts))){
-    split_template<-append(split_template, inserts[i], after=j)
-    j = j + 2
-  }
-  return(split_template)
-  
 }
 
 #count pairs in template
